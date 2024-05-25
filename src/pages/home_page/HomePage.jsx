f83d66bdb161d234a78d4f1287910b0292292bec
afrizal0811 <afrizalmaulana0811@gmail.com>
2024-05-25 15:28:07 +0700

    hide flags in home page

--- src/pages/home_page/HomePage.jsx
+++ src/pages/home_page/HomePage.jsx
@@ -1,39 +1,39 @@
-import { map } from 'lodash'
-import React, { useEffect, useState } from 'react'
-import AntdImage from '../../components/image/AntdImage'
+// import { map } from 'lodash'
+// import React, { useEffect, useState } from 'react'
+// import AntdImage from '../../components/image/AntdImage'
+// import AntdTooltip from '../../components/tooltip/AntdTooltip'
+// import { getApi } from '../../utilities/handleApi'
+import React from 'react'
 import AntdSearch from '../../components/search/AntdSearch'
-import AntdTooltip from '../../components/tooltip/AntdTooltip'
-import { getApi } from '../../utilities/handleApi'
 import './styles.css'
 
 const HomePage = () => {
-  const [countries, setCountries] = useState([])
-  const [flags, setFlags] = useState([])
-  console.log('flags :', flags)
+  // const [countries, setCountries] = useState([])
+  // const [flags, setFlags] = useState([])
 
-  useEffect(() => {
-    const fetch = async () => {
-      const url = `${process.env.REACT_APP_BASE_URL}/all?fields=name,flags`
-      setCountries(await getApi(url))
-    }
-    fetch()
-  }, [])
+  // useEffect(() => {
+  //   const fetch = async () => {
+  //     const url = `${process.env.REACT_APP_BASE_URL}/all?fields=name,flags`
+  //     setCountries(await getApi(url))
+  //   }
+  //   fetch()
+  // }, [])
 
-  useEffect(() => {
-    const countriesLength = countries.length
-    if (countriesLength > 0) {
-      for (var i = 1; i <= 5; i++) {
-        const randomNum = Math.floor(Math.random() * countriesLength) + 1
-        setFlags((prev) => [...prev, countries[randomNum]])
-      }
-    }
-  }, [countries])
+  // useEffect(() => {
+  //   const countriesLength = countries.length
+  //   if (countriesLength > 0) {
+  //     for (var i = 1; i <= 5; i++) {
+  //       const randomNum = Math.floor(Math.random() * countriesLength) + 1
+  //       setFlags((prev) => [...prev, countries[randomNum]])
+  //     }
+  //   }
+  // }, [countries])
 
   return (
     <div className='home-container'>
       <div className='home-content'>
         <h1>Country</h1>
-        <div className='flag-container'>
+        {/* <div className='flag-container'>
           {map(flags, (flag) => (
             <AntdTooltip text={flag.name.common}>
               <div>
@@ -47,7 +47,7 @@ const HomePage = () => {
               </div>
             </AntdTooltip>
           ))}
-        </div>
+        </div> */}
         <AntdSearch />
       </div>
     </div>
